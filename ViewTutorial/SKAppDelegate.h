251a53d9c21c160eee3eecb6b7ee216e21ce547b
sakko <saklism@gmail.com>
2013-12-03 19:53:42 +0700

    first commit

---
+++ ViewTutorial/SKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SKAppDelegate.h
+//  ViewTutorial
+//
+//  Created by SaKKo on 12/3/13.
+//  Copyright (c) 2013 SaKKo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViewTutorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ViewTutorial
+//
+//  Created by SaKKo on 12/3/13.
+//  Copyright (c) 2013 SaKKo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SKAppDelegate class]));
+    }
+}
